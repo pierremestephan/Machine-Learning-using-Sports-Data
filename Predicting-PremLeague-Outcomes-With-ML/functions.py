3868f9832601d1d9553f1c05f37008241455fdff
Pierre Estephan <pierremestephan@gmail.com>
2021-02-09 21:32:34 -0700

    Pull players,events, and team dfs

--- Predicting-PremLeague-Outcomes-With-ML/functions.py
+++ Predicting-PremLeague-Outcomes-With-ML/functions.py
@@ -1,2 +1,18 @@
 import numpy as np
 import pandas as pd
+import requests
+import json
+
+def get(url):
+    response = requests.get(url)
+    return json.loads(response.content)
+
+def build_dataframes(response):
+    players = response['elements']
+    teams = response['teams']
+    events = response['events']
+
+    players_df = pd.DataFrame(players)
+    teams_df = pd.DataFrame(teams)
+    events_df = pd.DataFrame(events)
+    return players_df,teams_df,events_df
