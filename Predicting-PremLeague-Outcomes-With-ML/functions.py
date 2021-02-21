4cd34677af27003c368f2c57ac613debdf95071e
Pierre Estephan <pierremestephan@gmail.com>
2021-02-21 07:41:36 -0700

    Moved metrics to it's own function

--- Predicting-PremLeague-Outcomes-With-ML/functions.py
+++ Predicting-PremLeague-Outcomes-With-ML/functions.py
@@ -3,3 +3,24 @@ import pandas as pd
 import requests
 import json
 
+def metrics(df, y_pred):
+    df.loc[:, 'HomeWinPerc'] = y_pred[0]
+    df.loc[:, 'DrawPerc'] = y_pred[1]
+    df.loc[:, 'AwayPerc'] = y_pred[2]
+
+    conditions = [
+        (df['HomeWinPerc'] >= df['DrawPerc']) & (df['HomeWinPerc'] >= df['AwayPerc']), #Home Condition
+        (df['HomeWinPerc'] <= df['DrawPerc']) & (df['DrawPerc'] >= df['AwayPerc']), #Draw Condition
+        (df['HomeWinPerc'] <= df['DrawPerc']) & (df['DrawPerc'] <= df['AwayPerc']) #Away Condition
+    ]
+
+    values = [0, 1, 2]
+
+    df["PredResult"] = np.select(conditions, values)
+    df['Comparison'] = np.where(df["FTR"] == df["PredResult"], True, False)
+
+    print((df['Comparison'].value_counts()/df['Comparison'].count())*100)
+    print((df['FTR'].value_counts()/df['FTR'].count())*100)
+    print((df['PredResult'].value_counts()/df['PredResult'].count())*100)
+
+    
